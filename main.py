037888dfcc5b9836e15f72cc41e5875cdd107a17
rob <robls@niito.com>
2021-08-18 18:37:23 +0100

    further boilerplate apis in fastapi

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello, and welcome to the Pokedex :-)"}
+
+@app.get("/pokemon/{pokemon_name}")
+async def pokemon(pokemon_name):
+    return {"message": f"Hello, this is {pokemon_name}"}
+
+@app.get("/pokemon/translated/{pokemon_name}")
+async def pokemon_transalted(pokemon_name):
+    return {"message": f"Hello, this is {pokemon_name} wityh the translated stuff"}
